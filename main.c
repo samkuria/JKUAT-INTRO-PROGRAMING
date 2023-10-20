bc64c31d24a71a41a653de77566255139fbb6dae
samkuria <145743807+samkuria@users.noreply.github.com>
2023-10-20 05:09:41 -0700

    looping

--- main.c
+++ main.c
@@ -3,14 +3,26 @@
 
 int main()
 {
-    int a, b, c, d ;
-    printf("Enter the first value");
-    scanf("%d", &a);
-    printf("Enter the second value");
-    scanf("%d", &b);
-    printf("Enter the third value");
-    scanf("%d", &c);
-    d = a + b+ c;
-    printf("%d + %d+ %d = %d\n", a, b, c, d);
+    float first_value , second_value, third_value;
+    printf("Enter the first value:");
+    scanf("%f" ,&first_value);
+    printf("Enter the second value:");
+    scanf("%f", &second_value);
+    printf("Enter the increment value:");
+    scanf("%f", &third_value);
+    while ( first_value <= 100)
+    {
+        if ((first_value > 98.6)&&(second_value < 98.6))
+        {
+            printf("%6.2f degrees F = %6.2f degrees C\n",
+                   98.6, (98.6 -32.0) * 5.0 / 9.0);
+        }
+        printf("%6.2f degrees F = %6.2f degrees C\n",
+               first_value,(first_value - 32.0) * 5.0 / 9.0);
+               second_value= first_value;
+               first_value= first_value + third_value;
+
+    }
+
     return 0;
 }
