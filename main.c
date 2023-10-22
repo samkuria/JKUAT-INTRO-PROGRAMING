5bde9dd63b4258342b6c6bc2841f306906276f1e
samkuria <145743807+samkuria@users.noreply.github.com>
2023-10-22 08:55:29 -0700

    arrays

--- main.c
+++ main.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    float first_value , second_value, third_value;
-    printf("Enter the first value:");
-    scanf("%f" ,&first_value);
-    printf("Enter the second value:");
-    scanf("%f", &second_value);
-    printf("Enter the increment value:");
-    scanf("%f", &third_value);
-    while ( first_value <= 100)
+    int a[5]={5,10,15,20,25};
+    int i;
+    for(i = 0; i < 5; i++)
     {
-        if ((first_value > 98.6)&&(second_value < 98.6))
-        {
-            printf("%6.2f degrees F = %6.2f degrees C\n",
-                   98.6, (98.6 -32.0) * 5.0 / 9.0);
-        }
-        printf("%6.2f degrees F = %6.2f degrees C\n",
-               first_value,(first_value - 32.0) * 5.0 / 9.0);
-               second_value= first_value;
-               first_value= first_value + third_value;
-
+        printf("%d\n", a[i]);
     }
 
     return 0;
